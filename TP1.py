29de34ab2d6de2f78b00857047dd68ee85cbddc9
Hamza Abbad <hamza.abbad@gmail.com>
2016-10-08 22:18:15 +0100

    Ajouter l'ordre
    Les clauses sont maintenant ordonnées.

--- TP1.py
+++ TP1.py
@@ -1,6 +1,7 @@
 import re
 import sys
 import pycosat
+from numbers import Real, Integral
 
 _read_error_msg = 'Impossible de lire le fichier'
 _usage_msg = 'Usage : {} <{}> <{}>'
@@ -15,7 +16,7 @@ def load_weighted_knowledge_base(file_path):
     """
     Return the content of a WCNF file.
     :param file_path: The path of the WCNF file.
-    :return: A list of lists where each inner list contains the weight and the variables of a DNF clause.
+    :return: A sorted list of WeightedClause.
     """
     with open(file_path) as kb_file:
         line_regexp = re.compile('\d+(?:\.\d+)?\s+(?:(-?\d+)\s+)+0')
@@ -27,10 +28,9 @@ def load_weighted_knowledge_base(file_path):
                 continue  # Skip empty lines, comments and header
             elif re.match(line_regexp, line):
                 line_variables = re.split(separator_regexp, line)
-                line_variables[0] = float(line_variables[0])
-                line_variables[1:] = [int(x) for x in line_variables[1:-1]]
-                weighted_clauses.append(line_variables)  # All variables except the 0
-    return weighted_clauses
+                weighted_clauses.append(WeightedClause(weight=float(line_variables[0]),
+                                                       dnf_clause=[int(x) for x in line_variables[1:-1]]))
+    return sorted(weighted_clauses)
 
 
 def negation(dnf_clause):
@@ -45,21 +45,71 @@ def negation(dnf_clause):
 def inconsistency_degree(knowledge_base_wc, clause):
     """
     Return the inconsistency degree of knowledge_base_wc union the negation of clause using the refutation.
-    :param knowledge_base_wc: A list of lists where each list contains the weight and the variables (Sigma).
+    :param knowledge_base_wc: A list of WeightedClause (Sigma).
     :param clause: A list containing variables (Phi).
     :return: The value of inconsistency Val(Sigma U neg(Phi)).
     """
+    assert isinstance(knowledge_base_wc, list) and all([isinstance(x, WeightedClause) for x in knowledge_base_wc])
     r = 0  # Just an initialisation
     l = 0
     u = len(knowledge_base_wc)
-    knowledge_base_c = [x[1:] for x in knowledge_base_wc]  # Remove the weights
     while l < u:
         r = (l + u) // 2  # Integer division
-        if isinstance(pycosat.solve(knowledge_base_c[r:u] + negation(clause)), list):  # Picosat found a solution
+        if isinstance(pycosat.solve([list(x) for x in knowledge_base_wc[r:u]] + negation(clause)), list):  # Picosat found a solution
             u = r - 1
         else:
             l = r
-    return knowledge_base_wc[r][0]  # Weight of the clause
+    return knowledge_base_wc[r].weight  # Weight of the clause
+
+
+class WeightedClause:
+    """
+    Represent a DNF clause with an associated weight.
+
+    Inspired from an idea by Abdelkarim (Walid) Boumehdi :)
+    """
+
+    def __init__(self, weight, dnf_clause):
+        assert isinstance(weight, Real)
+        self.weight = weight
+        assert isinstance(dnf_clause, list) and all([isinstance(x, Integral) for x in dnf_clause])
+        self.clause = dnf_clause
+
+    def __eq__(self, other):
+        assert isinstance(other, WeightedClause)
+        return self.weight == other.weight
+
+    def __lt__(self, other):
+        assert isinstance(other, WeightedClause)
+        return self.weight < other.weight
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __getitem__(self, item):
+        return self.clause[item]
+
+    def __len__(self):
+        return len(self.clause)
+
+    def __str__(self):
+        return '(' + str(self.clause) + ', ' + str(self.weight) + ')'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __iter__(self):
+        return iter(self.clause)
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
