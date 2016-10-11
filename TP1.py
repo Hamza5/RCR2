ecdeaaf70f62641e1e5470970310b0e112a222cb
Hamza Abbad <hamza.abbad@gmail.com>
2016-10-11 17:42:43 +0100

    Ajouter le support des strates
    Ajouter une classe qui gère les strates dans la base de connaissances.

--- TP1.py
+++ TP1.py
@@ -10,13 +10,14 @@ _dnf_msg = 'Clause FND'
 _sigma_msg = 'Sigma ='
 _phi_msg = 'Phi ='
 _val_msg = 'Val(Phi, Sigma) ='
+_interest_msg = 'Variable ='
 
 
 def load_weighted_knowledge_base(file_path):
     """
     Return the content of a WCNF file.
     :param file_path: The path of the WCNF file.
-    :return: A sorted list of WeightedClause.
+    :return: A WeightedKnowledgeBase.
     """
     with open(file_path) as kb_file:
         line_regexp = re.compile('\d+(?:\.\d+)?\s+(?:(-?\d+)\s+)+0')
@@ -28,9 +29,8 @@ def load_weighted_knowledge_base(file_path):
                 continue  # Skip empty lines, comments and header
             elif re.match(line_regexp, line):
                 line_variables = re.split(separator_regexp, line)
-                weighted_clauses.append(WeightedClause(weight=float(line_variables[0]),
-                                                       dnf_clause=[int(x) for x in line_variables[1:-1]]))
-    return sorted(weighted_clauses)
+                weighted_clauses.append([float(line_variables[0])] + [int(x) for x in line_variables[1:-1]])
+    return WeightedKnowledgeBase(weighted_clauses)
 
 
 def negation(dnf_clause):
@@ -45,21 +45,22 @@ def negation(dnf_clause):
 def inconsistency_degree(knowledge_base_wc, clause):
     """
     Return the inconsistency degree of knowledge_base_wc union the negation of clause using the refutation.
-    :param knowledge_base_wc: A list of WeightedClause (Sigma).
+    :param knowledge_base_wc: A WeightedKnowledgeBase (Sigma).
     :param clause: A list containing variables (Phi).
     :return: The value of inconsistency Val(Sigma U neg(Phi)).
     """
-    assert isinstance(knowledge_base_wc, list) and all([isinstance(x, WeightedClause) for x in knowledge_base_wc])
+    assert isinstance(knowledge_base_wc, WeightedKnowledgeBase)
     r = 0  # Just an initialisation
     l = 0
     u = len(knowledge_base_wc)
     while l < u:
         r = (l + u) // 2  # Integer division
-        if isinstance(pycosat.solve([list(x) for x in knowledge_base_wc[r:u]] + negation(clause)), list):  # Picosat found a solution
+        if isinstance(pycosat.solve([list(x) for x in knowledge_base_wc[r:u]] + negation(clause)), list):
+            # Picosat found a solution
             u = r - 1
         else:
             l = r
-    return knowledge_base_wc[r].weight  # Weight of the clause
+    return knowledge_base_wc.weights[r]  # Weight of the clause
 
 
 class WeightedClause:
@@ -111,19 +112,48 @@ class WeightedClause:
         return iter(self.clause)
 
 
+class WeightedKnowledgeBase:
+    """
+    Represent a CNF base of weighted DNF clauses.
+    """
+
+    def __init__(self, kb_wc=list()):
+        assert isinstance(kb_wc, list) and all([isinstance(x, list) for x in kb_wc])
+        self.clauses = {}
+        for x in kb_wc:
+            weighted_clause = WeightedClause(x[0], x[1:])
+            self.add_clause(weighted_clause)
+        self.weights = [w for w in sorted(self.clauses.keys())]
+
+    def add_clause(self, w_clause):
+        assert isinstance(w_clause, WeightedClause)
+        if w_clause.weight not in self.clauses:
+            self.clauses[w_clause.weight] = [w_clause]
+        else:
+            self.clauses[w_clause.weight].append(w_clause)
+
+    def __len__(self):
+        return len(self.weights)
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            items = []
+            for i in range(item.start, item.stop):
+                items += self.clauses[self.weights[i]]
+            return [list(x) for x in items]
+        else:
+            return [list(x) for x in self.clauses[self.weights[item]]]
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         try:
             Sigma = load_weighted_knowledge_base(sys.argv[1])  # WCNF file
-            print(_sigma_msg, end=' ')
-            if len(Sigma) > 10:
-                print('[', str(Sigma[:3])[1:-1], '...', str(Sigma[-3:])[1:-1], ']')
-            else:
-                print(Sigma)
             Phi = [int(x) for x in re.split('\s+', sys.argv[2])]  # DNF clause
             print(_phi_msg, Phi)
             val = inconsistency_degree(Sigma, Phi)
             print(_val_msg, val)
+            print(_interest_msg, Sigma.clauses[val])
         except FileNotFoundError as e:
             print(_read_error_msg, e.filename, file=sys.stderr)
     else:
